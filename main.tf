44dd6a80cf9d40cafb71163bd218d45bd382c881
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-06-06 11:02:31 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "wordpress_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.public_security_group.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_session_manager_profile.name
-  user_data = filebase64("${var.setup_filename}")
+  user_data = filebase64("${var.setup_filename_linux}")
 
   tags = {
     Name = "my-first-web-app"
@@ -167,7 +167,7 @@ resource "aws_instance" "windows_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.public_security_group.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_session_manager_profile.name
-  user_data = filebase64("${var.setup_filename}")
+  user_data = filebase64("${var.setup_filename_windows}")
 
   tags = {
     Name = "my-first-web-app"
